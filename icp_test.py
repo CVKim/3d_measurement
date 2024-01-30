016466afd78d733b218c3226ebced5bbe84d19e0
Gwangyeol Kim <kim.gwangyeol@aiv.ai>
2024-01-30 14:49:34 +0900

    add nearest neighbors

--- icp_test.py
+++ icp_test.py
@@ -5,6 +5,48 @@ import open3d as o3d
 import cv2
 import os
 
+
+from sklearn.neighbors import NearestNeighbors
+
+def nearest_neighbor(sorce, target, n_neighbors=1):
+    neigh = NearestNeighbors(n_neighbors=n_neighbors)
+    neigh.fit(target)
+    distances, indices = neigh.kneighbors(sorce, return_distance=True)
+    return distances.ravel(), indices.ravel()
+
+
+def find_near_point(sorce, target):
+    # 근접점(최소거리)거리와 비교 매칭된 dst index 리스트들
+    minmum_dist_list = [];
+    minmum_idx_list = [];
+
+    # source 점(point) 하나씩 조회
+    for idx_s,(src_x,src_y,src_z) in enumerate(sorce): 
+        minmum_dist = -1.0;
+        minmum_idx = -1.0;
+
+        # target 점(point) 하나씩 조회
+        for idx_t,(tg_x,tg_y,tg_z) in enumerate(target):   
+            # source와 target point 간 거리계산  
+            dist = np.linalg.norm(np.array([tg_x,tg_y,tg_z])-np.array([src_x,src_y,src_z]));
+
+            # 만약 처음이면, 최소거리값과 매칭된 index로 정의
+            if(idx_t == 0):
+                minmum_dist = dist
+                minmum_idx = idx_t;
+            else: # 만약 처음이 아니고, 거리가 최소 거리값보다 작으면
+                if(minmum_dist>dist):
+                    # 최소값 거리값와 매칭 index 업데이트
+                    minmum_dist = dist;
+                    minmum_idx = idx_t;
+        
+        # source점과 거리가 가까운 target 점들의 거리와 매칭된 index 기록
+        minmum_dist_list.append(minmum_dist);
+        minmum_idx_list.append(minmum_idx);
+    
+    # 근접점(최소거리) 거리와 매칭된 index 반환
+    return np.array(minmum_dist_list), np.array(minmum_idx_list)
+
 def pcd_show(point_clouds=[]):
     show_list = [];
     for point_cloud in point_clouds:
@@ -48,3 +90,14 @@ if __name__ == "__main__":
     source=pcd_rotation(target,45.0,0,0);
 
     pcd_show([source,target])
+    
+    distances,indices=find_near_point(source,target);
+    print("near distances :",distances)
+    print("near indices :",indices)
+    
+    distances,indices=find_near_point(source,target);
+    distances2,indices2=nearest_neighbor(source,target);
+    print("near distances :",distances)
+    print("near indices :",indices)
+    print("near distances diff :",distances-distances2)
+    print("near indices diff:",indices-indices2)
